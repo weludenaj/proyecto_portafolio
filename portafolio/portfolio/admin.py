b6d880e1c9add978770f326217b461e1d06a52a3
weludenaj <weludenaj@gmail.com>
2022-12-13 08:39:25 -0500

    Delete portafolio/portfolio directory

--- portafolio/portfolio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Project
-# Register your models here.
-# Necesario para poder ver los campos ocultos
-class ProjectAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
-
-admin.site.register(Project, ProjectAdmin)
